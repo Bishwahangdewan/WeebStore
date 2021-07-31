8239052aaa71111fa1732a37790eba23d6277305
Bishwahang Dewan <bishwahangdewan123@gmail.com>
2021-07-31 12:03:02 +0530

    Sign in Page built

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 import { Switch, Route } from 'react-router-dom';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/signin-and-signup/signin-and-signup.component';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={Shop} />
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
